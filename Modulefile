1be7e413dab5a3a58ac2a0dd91736cbb2de7590b
Igor Galić <i.galic@brainsware.org>
2013-10-08 14:45:07 +0200

    explain rationale for why we do this the way we do it.

--- Modulefile
+++ Modulefile
@@ -13,7 +13,7 @@
 #   limitations under the License.
 
 name    'brainsware-crashdump'
-version '0.1.0'
+version '0.1.1'
 source 'https://github.com/Brainsware.org/puppet-crashdump'
 author 'brainsware'
 license 'Apache License, Version 2.0'
--- manifests/config.pp
+++ manifests/config.pp
@@ -20,6 +20,7 @@ class crashdump::config {
 
   case $::osfamily {
     'RedHat' : {
+      # XXX this will need an onlyif =>
       exec { 'update grub config':
         command  => 'grubby --update-kernel=ALL --args="crashkernel=128M"',
         pwd      => '/',
@@ -32,7 +33,11 @@ class crashdump::config {
         # This is a hack. We make sure that on Ubuntu the crashkernel line in
         # /etc/grub.d/10_linux isn't something that will just NOT work, see errata:
         #   https://wiki.ubuntu.com/Kernel/CrashdumpRecipe#Release_specific_notes
-        # replace:     GRUB_CMDLINE_EXTRA="$GRUB_CMDLINE_EXTRA crashkernel=384M-2G:64M,2G-:128M"
+        #
+        # Rationale: We could put this into /etc/default/grub but that would mean
+        # potentially messing with other people's grub config. Since there's no
+        # way of managing a single part of a single line in a configuration file
+        # or any sensible hooks for grub that everyone uses, we keep it simple.
         'Ubuntu' : {
           file_line { 'crashkernel=128M':
             path  => '/etc/grub.d/10_linux',
